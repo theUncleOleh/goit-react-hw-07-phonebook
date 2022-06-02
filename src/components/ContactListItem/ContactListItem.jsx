d4eb3339f12f11c0c4ad3e3cda7cadab43880d02
Oleh Semennykov <sumcue@gmail.com>
2022-06-02 13:44:48 +0300

    add new delete

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -5,7 +5,7 @@ import s from './ContactListItem.module.css';
 export default function ContactListItem({ id, name, phone }) {
   const [deleteContact, { isLoading: isDeliting }] = useDeleteContactMutation();
   const deleteListItem = id => {
-    deleteContact();
+    deleteContact(id);
     toast.success(`Contact ${name} successfuly delete`);
   };
   return (
@@ -17,7 +17,7 @@ export default function ContactListItem({ id, name, phone }) {
         type="button"
         disabled={isDeliting}
         className={s.button}
-        onClick={() => deleteListItem(id)}
+        onClick={deleteListItem}
       >
         {isDeliting ? (
           <ScaleLoader height={15} width={5} margin={2} />
